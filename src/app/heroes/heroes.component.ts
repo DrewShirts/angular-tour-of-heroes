d230bb02439b51400024f1a824bad6fc186e5db4
Drew Shirts <ShirtsDre@ldschurch.org>
2019-03-28 00:47:08 -0600

    TOH: Display a Heroes List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +9,18 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
   constructor() { }
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   ngOnInit() {
   }
